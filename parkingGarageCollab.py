c34bfb316a75dd81318b10a226012e4fa5edc872
Allyse Arucan <81602578+allyroo@users.noreply.github.com>
2021-06-05 09:27:39 -0700

    Add files via upload

---
+++ parkingGarageCollab.py
@@ -0,0 +1,8 @@
+class parkingGarage:
+
+    def __init__(self, tickets, parkingSpaces, currentTicket):
+        self.tickets = tickets
+        self.parkingSpaces = parkingSpaces
+        self.currentTicket = currentTicket
+
+    
